ce431e88adf9315c6e5f229584ad4cee416105be
chenchang <chang.chen@fintecher.cn>
2017-04-08 12:10:33 +0800

    修改文件

--- src/main/java/com/github/alex/validate/ValidateApplication.java
+++ src/main/java/com/github/alex/validate/ValidateApplication.java
@@ -1,12 +1,12 @@
-package com.github.chc123456;
+package com.github.alex.validate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ValidatingInputRestRequestApplication {
+public class ValidateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ValidatingInputRestRequestApplication.class, args);
+		SpringApplication.run(ValidateApplication.class, args);
 	}
 }
--- src/test/java/com/github/alex/validate/ValidateApplicationTests.java
+++ src/test/java/com/github/alex/validate/ValidateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.github.chc123456;
+package com.github.alex.validate;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ValidatingInputRestRequestApplicationTests {
+public class ValidateApplicationTests {
 
 	@Test
 	public void contextLoads() {
