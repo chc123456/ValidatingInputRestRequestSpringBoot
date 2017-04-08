0a0f46488d66c6ff546f4e05571d136916a667d0
chenchang <chang.chen@fintecher.cn>
2017-04-08 12:02:40 +0800

    create

---
+++ src/main/java/com/github/chc123456/ValidatingInputRestRequestApplication.java
@@ -0,0 +1,12 @@
+package com.github.chc123456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingInputRestRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatingInputRestRequestApplication.class, args);
+	}
+}
