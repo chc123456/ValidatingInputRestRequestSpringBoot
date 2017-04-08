a6955983b3e1a89bd95e7d586798dec7bd245cdd
chenchang <chang.chen@fintecher.cn>
2017-04-08 17:22:56 +0800

    增加email

--- src/main/java/com/github/alex/validate/model/User.java
+++ src/main/java/com/github/alex/validate/model/User.java
@@ -7,7 +7,7 @@ public class User {
     private Long id;
     private String username;
     private String password;
-
+    private String email;
     public Long getId() {
         return id;
     }
@@ -30,5 +30,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/main/java/com/github/alex/validate/model/UserRequest.java
+++ src/main/java/com/github/alex/validate/model/UserRequest.java
@@ -1,5 +1,7 @@
 package com.github.alex.validate.model;
 
+import org.hibernate.validator.constraints.Email;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -14,6 +16,9 @@ public class UserRequest {
     @NotNull
     @Size(min = 6, max = 30)
     private String password;
+    @Email
+    @NotNull
+    private String email;
 
 
     public Long getId() {
@@ -38,5 +43,13 @@ public class UserRequest {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
